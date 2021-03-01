d2a75e63ba9d8f638e3c1471882b15d7eaaebbc4
Smogo <aurelien.genois@gmail.com>
2021-03-01 15:09:32 +0100

    Add computer plays with random cells

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // players factory
 const player = (name, marker) => {
     let _points = 0;
+    let _isRobot = false;
     const getName = () => name;
     const setName = (newName) => {
         name = newName;
@@ -10,10 +11,14 @@ const player = (name, marker) => {
     const addPoints = () => {
         _points++;
     };
-    return { getName, setName, getMarker, getPoints, addPoints};
+    const getAi = () => _isRobot;
+    const setAi = () => {
+        _isRobot = true;
+    }
+    return { getName, setName, getMarker, getPoints, addPoints, getAi, setAi};
 }
 const player1 = player('Player 1', 'X');
-const player2 = player('Player 2', 'O'); // or AI
+const player2 = player('Player 2', 'O');
 
 // GameBoard
 const gameBoard = (() => {
@@ -30,6 +35,8 @@ const gameBoard = (() => {
 })();
 
 // TODO module for AI ?
+// TODO if AI, make the click event raises on a random ('.game-cells') 
+// TODO one that works, choose the game-cells with ID according to the best move
 
 // game play logic
 const playController = (() => {
@@ -47,6 +54,17 @@ const playController = (() => {
         [0,4,8],
         [2,4,6]
     ]; 
+    const _randomLeftCellIndex = () => {
+        let cellsIdLeft = []; // array for Id of empty cells
+        gameBoard.getArr().map((x, i) => {
+            if(x == '') {
+                cellsIdLeft.push(i);
+            }
+        } );
+        // random index number among the empty cells array
+        let randomCellNb = Math.floor(Math.random() * Math.floor(cellsIdLeft.length - 1));
+        return cellsIdLeft[randomCellNb];
+    }
     // deep comparison because arr.includes() only works for shallow comparison,
     const _checkWin = (playerPlays, currentPlayer) => {
         _isGameOver = _winStates.some(e => e.every(o => playerPlays.includes(o)));
@@ -56,32 +74,67 @@ const playController = (() => {
         displayDOMController.displayResult(_isGameOver, currentPlayer);
       };
     
-    const _updateCell = (cell, currentPlayer, currentPlayerPlays) => {
+    const _updateCell = (cell, cellId, currentPlayer, currentPlayerPlays) => {
         cell.textContent = currentPlayer.getMarker();
-        gameBoard.setCell(cell.id, currentPlayer.getMarker())
-        currentPlayerPlays.push(+cell.id);
+        gameBoard.setCell(cellId, currentPlayer.getMarker());
+        currentPlayerPlays.push(+cellId);
         _checkWin(currentPlayerPlays.sort((a,b) => a - b), currentPlayer);
-    };
+    }; 
+    // need to work for an click event or computer AI 
+    const aiPlay = (aiPlayer) => {
+        let randomCellIndex = _randomLeftCellIndex();
+        if(randomCellIndex!== undefined && !_isGameOver) {
+            if (aiPlayer == player1) { // player1 turn
+                _updateCell(displayDOMController.getGameCells()[randomCellIndex], randomCellIndex,player1, _player1plays);
+                ++_turnCounter;
+                if(player2.getAi()) { // if other player ai, need a timeOut intervalle (1s)
+                    setTimeout(function() {
+                        aiPlay(player2);
+                        }, 500);
+                }
+            } else if (aiPlayer == player2) { // player2 turn
+                _updateCell(displayDOMController.getGameCells()[randomCellIndex], randomCellIndex,player2, _player2plays);
+                ++_turnCounter;
+                if(player1.getAi()) { // if other player ai, need a timeOut intervalle (1s)
+                    setTimeout(function() {
+                        aiPlay(player1);
+                        }, 500);
+                }
+            }
+        }
+        
+    }
     const checkCell = (e) => {
         if(!e.target.textContent && !_isGameOver) { // if cell empty
             if ((_turnCounter % 2) === 0) { // player1 turn
-                _updateCell(e.target,player1, _player1plays);
+                _updateCell(e.target, e.target.id,player1, _player1plays);
+                ++_turnCounter;
+                if(player2.getAi()) { // if other player ai, need a timeOut intervalle (1s)
+                    setTimeout(function() {
+                        aiPlay(player2);
+                        }, 500);
+                }
             } else { // player2 turn
-                _updateCell(e.target,player2, _player2plays);
+                _updateCell(e.target, e.target.id,player2, _player2plays);
+                ++_turnCounter;
+                if(player1.getAi()) { // if other player ai, need a timeOut intervalle (1s)
+                    setTimeout(function() {
+                        aiPlay(player1);
+                        }, 500);
+                }
             }
-            ++_turnCounter;
         }
     };
     const newRound = () => {
         gameBoard.reset();
-        displayDOMController.initGameBoardToDom();
         _player1plays = [];
         _player2plays = [];
         _turnCounter = 0;
         _isGameOver = false;
+        displayDOMController.initGameBoardToDom();
     };
 
-    return {checkCell, newRound};
+    return {checkCell, newRound, aiPlay};
 })();
 
 // methods for display to the dom
@@ -98,7 +151,7 @@ const displayDOMController = ((doc) => {
     _score2Text.textContent = player2.getPoints();
     _restartButton.addEventListener('click', playController.newRound);
 
-    // homeForm => to an other module ??
+    // TODO homeForm => to an other module ??
     const _homeForm = doc.querySelector('#home-form');
     _homeForm.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -110,12 +163,16 @@ const displayDOMController = ((doc) => {
         _player2Text.textContent = player2.getName();
         
         if(_homeForm.querySelector('#robot-1').checked) {
-            console.log('1 is AI'); // define player1 as a AI
+            console.log('1 is AI'); 
+            player1.setAi(); // define player1 as a AI
         }
         if(_homeForm.querySelector('#robot-2').checked) {
-            console.log('2 is AI'); // define player2 as a AI
+            console.log('2 is AI'); 
+            player2.setAi(); // define player2 as a AI
         } 
-        
+
+        initGameBoardToDom(); // first init
+
         _game.removeAttribute('hidden');
         _homeForm.setAttribute('hidden', true);
     });
@@ -146,11 +203,13 @@ const displayDOMController = ((doc) => {
                     break;
             }
             let gameCell = _createCell(cellId++, x, cellColl++, cellRow);
-            gameCell.addEventListener('mousedown', playController.checkCell);
+            gameCell.addEventListener('click', playController.checkCell);
             _gameBoardGrid.appendChild(gameCell);
-        })
+        });
+        if(player1.getAi()) {
+            playController.aiPlay(player1);
+        }
     };
-    initGameBoardToDom(); // first init
 
     const displayResult = (gameOverCheck, player) => {
         if(gameOverCheck === 'tie') {
@@ -163,6 +222,8 @@ const displayDOMController = ((doc) => {
             // (player === player1) ? _score1Text.textContent++ : _score2Text.textContent++ ;
         }
     };
-    return { initGameBoardToDom, displayResult};
+
+    const getGameCells = () => [..._gameBoardGrid.querySelectorAll('.game-cells')];
+    return { initGameBoardToDom, displayResult, getGameCells};
 })(document);
 
